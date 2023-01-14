4529be12965c700cea8daea07944393f17f7f310
probitie <viacheslav.mashkov@gmail.com>
2023-01-14 12:47:22 +0200

    added test range indicator

--- scenes/main/UI.gd
+++ scenes/main/UI.gd
@@ -1,12 +1,25 @@
 extends CanvasLayer
 
+var tower_range = 350
+
 func set_tower_preview(tower_type, mouse_position):
 	var drag_tower = load("res://scenes/turrets/" + tower_type + ".tscn").instance()
 	drag_tower.set_name("DragTower")
 	drag_tower.modulate = Color("ad54ff3c")
 	
+	# create range overlay
+	var range_texture = Sprite.new()
+	range_texture.position =  Vector2(32, 32)
+	var scaling = tower_range / 600.0
+	range_texture.scale = Vector2(scaling, scaling)
+	var texture = load("res://assets/ui/range_overlay.png")
+	range_texture.texture = texture
+	range_texture.modulate = Color("ad54ff3c")
+	
+	
 	var control = Control.new()
 	control.add_child(drag_tower, true)
+	control.add_child(range_texture, true)
 	control.rect_position = mouse_position
 	control.set_name("TowerPreview")
 	add_child(control, true)
@@ -16,3 +29,5 @@ func update_tower_preview(new_position, color):
 	$TowerPreview.rect_position = new_position
 	if($TowerPreview/DragTower.modulate != Color(color)):
 		$TowerPreview/DragTower.modulate = Color(color)
+		$TowerPreview/Sprite.modulate = Color(color)
+		
