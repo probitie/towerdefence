2a75daaca7b42cfbddaf09aed7eaf9fd3b1dae54
probitie <viacheslav.mashkov@gmail.com>
2023-01-18 08:56:53 +0200

    healthbar

--- scenes/enemies/BlueTank.gd
+++ scenes/enemies/BlueTank.gd
@@ -3,14 +3,23 @@ extends PathFollow2D
 var speed = 150
 var hp = 50
 
+onready var health_bar = $HealthBar
+
+func _ready():
+	health_bar.max_value = hp
+	health_bar.value = hp
+	health_bar.set_as_toplevel(true) # disconnect position, rotation of the tank from hp bar
+
 func _physics_process(delta):
 	move(delta)
+	health_bar.set_position(position + Vector2(-22, -29))
 	
 func move(delta):
 	set_offset(get_offset() + speed * delta)
 
 func on_hit(damage):
 	hp -= damage
+	health_bar.value = hp
 	if hp <= 0:
 		on_destroy()
 
